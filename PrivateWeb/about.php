de0662c4509f443cdedc35165459b57272f34932
Dhifamutia21 <72093867+Dhifamutia21@users.noreply.github.com>
2020-09-30 01:10:15 +0700

    Add files via upload

---
+++ PrivateWeb/about.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Private Web</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+	<div class="container">
+		<div id="branding">
+			<h1>Private Web</h1>
+		</div>
+		<nav>
+			<ul>
+				<li class="active"><a href="index.php">Home</a> </li>
+				<li> <a href="about.php"> About </a></li>
+				<li> <a href="#"> Contact </a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<!-- FASILITAS MENARIK -->
+<section id="showcase">
+	<div class="container">
+		<h1>- About Me -</h1>
+	</div>
+</section>
+<section id="showcase2">
+	<div class="container">
+		<h1>Dhifa Mutia Wulan Sari</h1>
+		<p><b>
+			"Mahasiswi Universitas Islam Negeri Syarif Hidayatullah Jakarta, angkatan 2018 Program Studi Teknik Informatika	."
+		</b></p>
+	<div class="box">
+		<img src="img/p.png">
+	</div>
+	</div>
+</section>
+
+<!-- WFH -->
+<!-- <section id="showcase">
+	<div class="container">
+		<h1>- Work From Home Situation -</h1>
+		<form>
+			<input type="email" placeholder="Enter Email...">
+			<button class="buttonsubs">Subscribe</button>
+		</form>
+	</div>
+</section>
+
+<section id="boxes">
+	<div class="container">
+		<div class="box">
+			<img src="img/WFH.png">
+			<h3>WORK FROM HOME</h3>
+		</div>
+		<div class="box">
+			<p>
+				Kebijakan Work from Home (WFH) di masa pandemi coronavirus, menjadikan tantangan tersendiri bagi saya untuk tetap produktif walaupun di rumah saja. Memeanfaatkan banyak waktu dan mengisinya dengan banyak hal positif, walaupun tidak sesempurna itu, ada kalanya saya menggunakan waktu luang untuk scroll social media dan membuka banyak cerita komik pada app mobile untuk menghibur diri sendiri. selama masa pandemi saya mulai berfikir bagaimana caranya waktu luang saya tidak hanya digunakan untuk rebahan dan bermain social media, sampai akhirnya saya memiliki keinginan untuk selama masa pandemi ini saya bisa menghasilkan uang tanpa bekerja diluar, kalau difikir-fikir sekaligus saya bisa menabung. saya pun memutuskan untuk berjualan online dengan memanfaatkan semua social media yang saya punya, yang mulanya saya hanya memiliki niat agar mengisi waktu luang biasa dan sedikit-sedikit memiliki tambahan uang jajan, tapi karna saya sangat senang menjalaninya sampai akhirnya online shop yang saya bangun dari bulan mei lama-lama terus berkembang dan dikenal banyak orang. cukup senang bahkan sangat senang, akhirnya yang awalnya niat saya hanya ingin menjalaninya sampai waktu kulian online dimulai, sekarang saya terus konsisten menjalani small business saya itu tanpa mengganggu jam kuliah saya. anggap saja sebelum saya terjun ke dunia kerja saya sudah belajar me manage waktu, walau masih belum sepenuhnya baik dalam mengatur waktunya. selama perkuliahan mulai berjalan, saya merasa sedikit kewalahan dalam membagi waktu antara onlineshop dengan kuliah, namun lambat laun saya bisa membaginya walau waktu yang sebelumnya saya pakai untuk beristirahat, sekarang saya gunakan untuk packing order juga mengerjakan tugas kuliah saya. alhamdulillah selama wfh ini saya mendapat benyak pelajaran dalam mendapatkan rupiah dan kegiatan yang saya jalankan sangat bermanfaat. 
+			</p>
+		</div>
+	</div>
+</section>
+ -->
+<footer>
+	<p>
+		Private Web, Copyright @2020.
+	</p>
+</footer>
+
+</body>
+</html>
---
+++ PrivateWeb/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+	<title>Private Web</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<header>
+	<div class="container">
+		<div id="branding">
+			<h1>Private Web</h1>
+		</div>
+		<nav>
+			<ul>
+				<li class="active"><a href="index.php">Home</a> </li>
+				<li> <a href="about.php"> About </a></li>
+				<li> <a href="#"> Contact </a></li>
+			</ul>
+		</nav>
+	</div>
+</header>
+
+<!-- FASILITAS MENARIK -->
+<section id="showcase">
+	<div class="container">
+		<h1>- Fasilitas Menarik -</h1>
+	</div>
+</section>
+<section id="showcase2">
+	<div class="container">
+		<h1>Perpustakaan Utama<br/>UIN Jakarta</h1>
+		<p><b>
+			"Salah satu Fasilitas menarik versi saya yang ada di Universitas Islam Negeri (UIN) Syarif Hidayatullah Jakarta adalah perpustakaan utama, Letaknya tepat di depan Fakultas FIDIKOM. Mengapa hal itu menjadi salah satu Fasilitas menarik versi saya? karena, di tempat itu walaupun cukup sulit untuk mendapatkan tempat yang nyaman dan mendapatkan colokan tetapi unutk mengerjakan tugas sangat efektif dan efisien karena selain mendapat wifi gratis juga saya dapat dengan mudah mendapatkan referensi dari buku-buku yang ada di perpusatakaan, juga di tempat itu terasa lebih nyaman dan tenang saat mengerjakan tugas baik kelompok maupun individu, disambil mendengarkan musik melalui earphone. Buku-buku yang disediakan juga cukup lengkap dan cukup membantu. Didalamnya pun terdapat mushola, sehingga memudahkan mahasiswa termasuk saya pribadi ketika sudah masuk jam sholat wajib."
+		</b><br/><br/>- dhifamutia -</p>
+	</div>
+</section>
+
+<!-- WFH -->
+<section id="showcase">
+	<div class="container">
+		<h1>- Work From Home Situation -</h1>
+<!-- 		<form>
+			<input type="email" placeholder="Enter Email...">
+			<button class="buttonsubs">Subscribe</button>
+		</form> -->
+	</div>
+</section>
+
+<section id="boxes">
+	<div class="container">
+		<div class="box">
+			<img src="img/WFH.png">
+			<h3>WORK FROM HOME</h3>
+		</div>
+		<div class="box">
+			<p>
+				Kebijakan Work from Home (WFH) di masa pandemi coronavirus, menjadikan tantangan tersendiri bagi saya untuk tetap produktif walaupun di rumah saja. Memeanfaatkan banyak waktu dan mengisinya dengan banyak hal positif, walaupun tidak sesempurna itu, ada kalanya saya menggunakan waktu luang untuk scroll social media dan membuka banyak cerita komik pada app mobile untuk menghibur diri sendiri. selama masa pandemi saya mulai berfikir bagaimana caranya waktu luang saya tidak hanya digunakan untuk rebahan dan bermain social media, sampai akhirnya saya memiliki keinginan untuk selama masa pandemi ini saya bisa menghasilkan uang tanpa bekerja diluar, kalau difikir-fikir sekaligus saya bisa menabung. saya pun memutuskan untuk berjualan online dengan memanfaatkan semua social media yang saya punya, yang mulanya saya hanya memiliki niat agar mengisi waktu luang biasa dan sedikit-sedikit memiliki tambahan uang jajan, tapi karna saya sangat senang menjalaninya sampai akhirnya online shop yang saya bangun dari bulan mei lama-lama terus berkembang dan dikenal banyak orang. cukup senang bahkan sangat senang, akhirnya yang awalnya niat saya hanya ingin menjalaninya sampai waktu kulian online dimulai, sekarang saya terus konsisten menjalani small business saya itu tanpa mengganggu jam kuliah saya. anggap saja sebelum saya terjun ke dunia kerja saya sudah belajar me manage waktu, walau masih belum sepenuhnya baik dalam mengatur waktunya. selama perkuliahan mulai berjalan, saya merasa sedikit kewalahan dalam membagi waktu antara onlineshop dengan kuliah, namun lambat laun saya bisa membaginya walau waktu yang sebelumnya saya pakai untuk beristirahat, sekarang saya gunakan untuk packing order juga mengerjakan tugas kuliah saya. alhamdulillah selama wfh ini saya mendapat benyak pelajaran dalam mendapatkan rupiah dan kegiatan yang saya jalankan sangat bermanfaat. 
+			</p>
+		</div>
+	</div>
+</section>
+
+<footer>
+	<p>
+		Private Web, Copyright @2020.
+	</p>
+</footer>
+
+</body>
+</html>
